4210d3686e21e1b4fbc80f0deb394a5864d04ce1
amisewick <amilaswickramasinghe@gmail.com>
2016-07-28 14:06:17 +0530

    Initial commit for git repo

---
+++ src/helloWorld.js
@@ -0,0 +1,21 @@
+/**
+ * Created by amila on 7/27/16.
+ */
+
+var HelloWorld = React.createClass({
+    render: function() {
+        return (
+            <p>
+                Hello, <input type="text" placeholder="Your name here" />!
+                It is {this.props.date.toTimeString()}
+            </p>
+        );
+    }
+});
+
+setInterval(function() {
+    ReactDOM.render(
+        <HelloWorld date={new Date()} />,
+        document.getElementById('example')
+    );
+}, 500);
